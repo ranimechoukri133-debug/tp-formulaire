09fbcb7c3378c717d9320a053e84af274b31006c
Ranime <ranimechoukri133@gmail.com>
2026-04-13 22:13:07 +0100

    feat: structure HTML et initialisation des variables

---
+++ candidature.php
@@ -0,0 +1,21 @@
+<?php
+$prenom = '';
+$nom = '';
+$email = '';
+$age = '';
+$filiere = '';
+$motivation = '';
+$erreurs = [];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Candidature</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
